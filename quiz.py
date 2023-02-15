9ef74c853549abf3f26d174a692f15c67924dd2f
thqu1et <210103164@stu.sdu.edu.kz>
2023-02-15 15:13:26 +0600

    new chage

--- quiz.py
+++ quiz.py
@@ -10,14 +10,25 @@
 # n = int(input("Enter a number to find its factorial: "))
 # factorial(n)
 
-def is_even(num):
-    if num % 2 == 0:
-        print(f"{num} is even")
-    else:
-        print(f"{num} is odd")
+# def is_even(num):
+#     if num % 2 == 0:
+#         print(f"{num} is even")
+#     else:
+#         print(f"{num} is odd")
+#
+# num = int(input("Enter a number to check whether it's even or odd: "))
+# is_even(num)
 
-num = int(input("Enter a number to check whether it's even or odd: "))
-is_even(num)
+def remove_duplicates(lst):
+    new_list = []
+    for item in lst:
+        if item not in new_list:
+            new_list.append(item)
+    return new_list
+
+lst = input("Enter a list of elements separated by commas: ").split(",")
+new_lst = remove_duplicates(lst)
+print("List with duplicates removed:", new_lst)
 
 
 
