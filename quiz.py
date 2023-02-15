5f0009f6bc0e0e19a405264c8f7a000bbd183d09
thqu1et <210103164@stu.sdu.edu.kz>
2023-02-15 14:54:20 +0600

    new message
    new mess

---
+++ quiz.py
@@ -0,0 +1,37 @@
+# print("Hello world")
+#
+# x = 5
+# y=10
+# print(x+y)
+#
+# carname = "Volvo"
+#
+# q = "Hello world"
+# print(len(q))
+#
+# print(10==9) # False
+#
+# print(10*5)
+#
+# fruits=["apple" , "banana" , "cherry"]
+# fruits[0]="kivi"
+#
+# fruits1=["apple" , "banana"]
+#
+#
+# fruits2=["apple" , "banana" , "cherry"]
+#
+# # def my_function(): 10
+#
+#
+# x = 20.5 #float 12
+
+# i = 1 13
+# while i < 6:
+#   if i == 3:
+#     i += 1
+# print(i)
+
+
+
+
